5caf05f3c6b8e698b844e556b1758ad5f430c7ae
Anthony Holt <Anthony Holt>
2016-09-21 10:28:29 -0400

    getting some basic work in JavaScript

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,61 @@
+
+// var num = 7;
+// var newNum = 5; //this is camel casting
+// var newer_num = 10; //this is snake casing
+
+// //console.log() prints to the console
+
+// // operations with console.log
+// console.log (5+5);
+// //TODO - make a subtraction, multiplication, and division console.log()
+
+// console.log(5-5);
+// console.log(5*5);
+// console.log(5/5);
+// // modulus operator
+// console.log (10%5); //returns zero
+// console.log(4%3); // returns 1 3 goes into 4 once with a REMAINDER of 1
+// //TODO - make a modulus that returns zero and another that returns a remainder
+// console.log(200%2);
+// console.log(154%3);
+//TODO- try to make a variable that is assigned an operation and a console log it
+//example var num = 1+1
+//TODO - try to assign numbers to different variables and perform an operation to them - example var a=5; var = b; console.log(a*b);
+//TODO - try to assign numbers to two different variables then assign any operation on these two variables to a new  variable
+var a = 5;
+var b = 10;
+var c = 7; 
+var d =3;
+var e = 2+6;
+console.log(e);
+console.log(b-d);
+console.log(c=b-d);
+//shortcuts for some basic math
+e=e+1;
+console.log(e);
+e++;
+console.log(e);
+
+e=e-1;
+console.log(e);
+e--;
+console.log(e);
+
+e=e+10;
+console.log(e);
+e+=10;
+console.log(e);
+
+//TODO - take what you know from addition and apply it ot subtraction
+
+e=e-10;
+console.log(e);
+e-=10;
+console.log(e);
+
+//testing with multiplication and division
+
+e *=10;
+console.log(e);
+e /=10;
+console.log(e);
