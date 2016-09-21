013616ef04779cc58ae33ea5c20da9cb75ab1dc4
Anthony Holt <Anthony Holt>
2016-09-21 11:14:01 -0400

    working on JavaScript Strings

---
+++ jsbasics/0.02_strings.js
@@ -0,0 +1,29 @@
+var birthCity="Pheonix";
+var birthState= "Arizona";
+console.log(birthCity);
+var username= "Anthony Holt";
+var password= "password";
+var tweet ="Kanye is broke again";
+var facebookPost= "I love coding!";
+var instaCaption= "#Awesome";
+console.log(tweet);
+
+console.log("Hello World");
+console.log("")
+console.log("Hey you guys!");
+
+//CONCATENATION
+console.log(birthCity+",", birthState);
+console.log(birthCity +", "+ birthState);
+//challenge: create 2 variables. year you graduated HS. & the HS you graduated from.
+var highSchool="St. Johns High School";
+var highSchoolYear=2011;
+console.log("I graduated from "+ highSchool +" in "+highSchoolYear+".");
+
+//create one variable that holds a string that prints out three other variables. 
+//the three other variables should be the make, model and year of your car.
+var make="Pontiac";
+var model=" Vibe ";
+var carYear= 2003 ;
+var anthonyCar= make+model+carYear;
+console.log(anthonyCar);
