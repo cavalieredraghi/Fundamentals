4ff871a86bbe938ce080efa0cc8a05313245971f
Anthony Holt <Anthony Holt>
2016-09-22 11:25:19 -0400

    more work in JavaScript with HTML.

--- jsbasics/1.0_theDOM.js
+++ jsbasics/1.0_theDOM.js
@@ -1,4 +1,29 @@
 var hobbyList= document.getElementById("hobby-list");
 console.log(hobbyList);
 var hobbies = document.getElementsByClassName("hobby");
-console.log(hobbies);
+console.log(hobbies[0].innerHTML); //an array
+console.log(hobbies[1].innerHTML);
+console.log(hobbies[2].innerHTML);
+console.log(hobbies[3].innerHTML);
+console.log(hobbies[4].innerHTML);
+
+var music= document.getElementById("music");
+var worst = document.getElementById("worst");
+var best = document.getElementById("best");
+var favorite = document.getElementById("favorite");
+function favoriteSong(){
+	document.getElementById("favorite").innerHTML ="Freebird!!!";
+}
+
+favoriteSong();
+function worstArtist(){
+	document.getElementById("worst").innerHTML = "Kayne West";
+}
+
+worstArtist();
+
+function bestBand(){
+	document.getElementById("best").innerHTML = "Linkin Park";
+}
+
+bestBand ();
